9e665e2fa566023d8e433fba770c229617f4d1fa
Isaias Lucena <isaiiaslucena@gmail.com>
2022-06-08 23:37:18 -0300

    Created search bar; Created css for search bar; Created List component.

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import "./styles.css";
+
 const Search = () => {
+  const handleClick = () => {
+    return null;
+  }
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    return null;
+  };
+  const [value, setValue] = useState(null);
+  console.log("button value", value);
   return (
-    <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
-  Click me
-</button>
+    <div className="wrapper grid place-items-center h-screen">
+      <div className="flex">
+        <input
+          type="text"
+          className="inpt-bar focus:outline-none focus:ring focus:ring-red-300"
+          placeholder="Digite um autor, título ou assunto"
+          value={value}
+          onChange={handleChange}
+        />
+        <button
+          type="button"
+          onClick={handleClick}
+          className="btn bg-red-800 hover:bg-red-900 focus:outline-none focus:ring focus:ring-red-300"
+        >
+          SEARCH
+        </button>
+      </div>
+    </div>
   );
 };
 
